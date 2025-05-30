916b2132b35500d7213e3c0c79f88eb92f72b3f0
Guillaume Bougard <gbougard@teclib.com>
2025-05-30 10:22:15 +0200

    build: Update MSI signing handling

--- contrib/windows/code-signing.pl
+++ contrib/windows/code-signing.pl
@@ -10,12 +10,25 @@ die "missing git bash environment\n" unless -e bash;
 # Script assuming a private.key file has been created in the current folder
 die "missing private.key file\n" unless -e "private.key";
 
-my ($source, $signed) = @ARGV;
+my ($source) = @ARGV;
 
 $source =~ s/\\/\//g;
-$signed =~ s/\\/\//g;
+
+my $signed = $source =~ /^(.*)\.(\w+)$/ ? "$1-signed.$2" : $source . "-signed";
 
 my @ssh = ("ssh", "-T", "-o", "StrictHostKeyChecking=yes", "-i", "private.key");
 push @ssh, "codesign", "codesign", $source;
 
-system(bash, "-c", "cat '$source' | @ssh >'$signed'");
+system(bash, "-c", "cat '$source' | @ssh >'$signed'") == 0
+    or die "Failed to sign $source\n";
+
+die "Failed to sign $source: empty result\n"
+    unless -s $signed;
+
+unlink $source
+    or die "Failed to remove $source: $!\n";
+
+rename $signed, $source
+    or die "Failed to rename $signed into $source: $!\n";
+
+exit(0);
